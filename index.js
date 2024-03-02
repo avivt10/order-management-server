fe86f58eedb2c59644873acddba373a00c8f0078
Aviv Turgeman <avivturgeman10@gmail.com>
2024-03-02 15:28:41 +0200

    add cors for project

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const ordersRoutes = require("./routes/orders-routes")
 const HttpError = require("./models/http-error");
 const mongoose = require("mongoose")
 mongoose.set("strictQuery", false)
+const cors = require("cors")
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`run port : ${PORT}`)
 });
 
-
+app.use(cors())
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5174');
+  const allowedOrigin = req.headers.origin || 'http://localhost:5173';
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
--- routes/orders-routes.js
+++ routes/orders-routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const orderController = require("../controller/order-controller")
 const router = express.Router()
+
 router.post('/createOrder', orderController.createOrder)
 router.get('/getOrders', orderController.getOrders)
 router.delete('/deleteOrder', orderController.deleteOrder)
